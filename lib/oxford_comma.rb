78e4afad1aa85e75288585bbedefa8c63143fb23
Abagail Hudson <abagailanne@gmail.com>
2019-05-05 04:00:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
  elsif array.length ==2
    array.join(' and ')
  else
-    str = ", and " + array.pop
-    str = array.join(", ") << str
+    newString = ", and " + array.pop
+    newString = array.join(", ") << newString
   end
 end
