37ccd4559ffd1256b6c68710de2e3a9417cb7b34
Abagail Hudson <abagailanne@gmail.com>
2019-05-05 03:46:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,4 @@
 def oxford_comma(array)
-  default_words_connector     = ", "
-     default_two_words_connector = " and "
-     default_last_word_connector = ", and "
-
-     case length
-       when 0
-         ""
-       when 1
-         self[0].to_s.dup
-       when 2
-         "#{self[0]}#{options[:two_words_connector]}#{self[1]}"
-       else
-         "#{self[0...-1].join(options[:words_connector])}#{options[:last_word_connector]}#{self[-1]}"
-     end
+ if array.length < 2
+   return array
 end
