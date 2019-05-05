e8faf7420a76ba5c0947da8ebbf00f272179358d
Abagail Hudson <abagailanne@gmail.com>
2019-05-05 03:41:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
- return array.to_sentence
+  default_words_connector     = ", "
+     default_two_words_connector = " and "
+     default_last_word_connector = ", and "
+ 
+     case length
+       when 0
+         ""
+       when 1
+         self[0].to_s.dup
+       when 2
+         "#{self[0]}#{options[:two_words_connector]}#{self[1]}"
+       else
+         "#{self[0...-1].join(options[:words_connector])}#{options[:last_word_connector]}#{self[-1]}"
+     end
 end
